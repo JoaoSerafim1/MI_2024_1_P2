6c054f2e6f8c5b98a072f952766c299237169142
JoaoSerafim1 <joaolucascademico@gmail.com>
2024-10-15 13:28:41 -0300

    fix: Proper commentary implementation for address_map_arm.s

--- address_map_arm.s
+++ address_map_arm.s
@@ -1,9 +1,9 @@
-/*BASE ADDRESS*/
+@BASE ADDRESS
         .equ    ALT_LWFPGASLVS_OFST,    0xFF200000
         .equ    HW_REGS_BASE,           0xFC000000
         .equ    HW_REGS_SPAN,           0x04000000
 
-/*GPU*/
+@GPU
         .equ    DATA_B,                 0x70
         .equ    DATA_A,                 0x80
         .equ    RESET_PULSECOUNTER,     0x90
@@ -11,7 +11,7 @@
         .equ    WRFULL,                 0xB0
         .equ    WRREG,                  0xC0
 
-/*LEDS*/
+@LEDS
         .equ    HEX5_BASE,              0x10
         .equ    HEX4_BASE,              0x20
         .equ    HEX3_BASE,              0x30
@@ -19,5 +19,5 @@
         .equ    HEX1_BASE,              0x50
         .equ    HEX0_BASE,              0x60
 
-/*PUSH_BUTTONS*/
+@PUSH_BUTTONS
         .equ    KEYS_BASE,              0x0
