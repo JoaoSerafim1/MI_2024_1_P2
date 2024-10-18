4deb6944e42bd7392b26da2c69b48a84dabe2f53
JoaoSerafim1 <129481962+JoaoSerafim1@users.noreply.github.com>
2024-10-18 17:29:45 -0300

    feat: Adding WBM and DP (under progress)

--- LED_assembly.s
+++ LED_assembly.s
@@ -9,33 +9,41 @@ _start:
     MOV R7, #5
     SVC 0 @ Chama o sistema
     
-    MOV R4, R0 @ Copia o caminho de paginacao para o registro que corresponde ao argumento da chamada de sistema
-    LDR R10, =ALT_LWFPGASLVS_OFST
-    LDR R5, [R10]
+    MOV R10, R0
+    LDR R9, =ALT_LWFPGASLVS_OFST
 
     MOV R0, #0 @ Volta o registro R0 para 0, ja que este sera usado para avaliar se houve erro
     MOV R1, #4096 @ Tamanho do pagina
     MOV R2, #3 @ Leitura e escrita
     MOV R3, #1 @ Modo MAP_SHARED, automaticamente guarda todas as alteracoes feitas na regiao mapeada na pagina
+    MOV R4, R10 @ Copia o caminho de paginacao para o registro que corresponde ao argumento da chamada de sistema 
+    LDR R5, [R9]
     MOV R7, #192 @ Codigo da chamada do sistema para mapeamento (mmap2)
     SVC 0 @ Chama o sistema
-    CMP R0, #0
-    BGT BRIDGE_ERROR
+    CMP R0, #-1
+    BEQ BRIDGE_ERROR
+
+    DATA_A:             MOV R1, #0x80
+    DATA_B:             MOV R2, #0x70
+    WRREG:              MOV R3, #0xc0
+    WRFULL:             MOV R4, #0xb0
+    SCREEN:             MOV R5, #0xa0
+    RESET_PULSECOUNTER: MOV R6, #0x90
+
+    MOV R9, #0b0011 @ Codigo de definir forma (DP)
+    LDR R10, =SQR_POL @ pega o endereco da variavel
+    LSL R10, R10, #4 @ Shift logico para a esquerda em 4 casa binarias
+    ADD R9, R9, R10 @ Adiciona os dois valores binarios
+
+    MOV R7, #0b0010 @ Codigo de modificar background (WBM)
     
-    MOV R10, R0
-    MOV R1, #0x0
-    MOV R2, #0x60
+    MOV R7, #0b111 @ Red = 7
+    MOV R8, #0b000000 @ Green = 0
+    ADD R7, R7, R8
+    MOV R8, #0b000000000 @ Blue = 0
+    ADD R7, R7, R8
 
-DISPLAY_KEY:
-    
-    ADD R3, R10, R1
-    ADD R4, R10, R2
 
-    LDR R6, [R4, #0] @ Carrega o valor atual dos botoes para um registro
-    STR R6, [R5, #0] @ Guarda o valor do registro no endereco de memoria do conjunto de LEDs 0
-    B DISPLAY_KEY @ Repete os dois comandos anteriores e este mesmo
-
-    MOV R0, #0 @ (Codigo de saida do programa 0)
     B END_OF_CODE @ Vai ao final do codigo, nao chega aqui, porem ja esta para redirecionar qualquer codigo que chegar ao final da execucao
 
 BRIDGE_ERROR:
@@ -52,10 +60,7 @@ END_OF_CODE:
     SVC 0 @ Chama o sistema
 
 .data
-    ALT_LWFPGASLVS_OFST:    .word   0xFF200000
-    HW_REGS_BASE:           .word   0xFC000000
-    HW_REGS_SPAN:           .word   0x04000000
+    ALT_LWFPGASLVS_OFST:    .word   0xff200
     pagingfolder:           .asciz  "/dev/mem"
     errmsgbridge:           .ascii  "Erro: Nao foi possivel estabelecer a conexao HPS-FPGA.\n"
-    @.equ HW_KEYS_BASE,      #0x0
-    @.equ HW_HEX0_BASE,      #0x60
+    SQR_POL:                .byte
